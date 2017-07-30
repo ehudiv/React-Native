11807256bd9d1bf4439520b06e10aabde1a615e0
ehudiv <ehud.iv1@gmail.com>
2017-07-30 14:58:25 +0300

    update

---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+    {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomwidth: 0,
+    shadowcolor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shaddowOpacity: 0.1,
+    shaddowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+
+  }
+};
+
+export default Card;
